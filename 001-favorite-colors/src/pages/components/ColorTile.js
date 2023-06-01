7228e2aab40d00148d37cdbead91db1177c39be3
Andrew Pethoud <andrewpethoud@gmail.com>
2023-05-31 20:34:42 -0700

    feat(001:app): change default font to Overpass

--- 001-favorite-colors/src/pages/components/ColorTile.js
+++ 001-favorite-colors/src/pages/components/ColorTile.js
@@ -1,6 +1,6 @@
 export default function ColorTile({ color }) {
   return (
-    <div style={{ background: color, padding: 12, margin: 4 }}>
+    <div style={{ background: color, padding: 12, margin: 4, borderRadius: 8 }}>
       <div>{color}</div>
     </div>
   )
