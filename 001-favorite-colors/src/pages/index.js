cc19a264fdf3cdd2eb187ce7b3782921dddb4761
Andrew Pethoud <andrewpethoud@gmail.com>
2023-05-31 19:22:04 -0700

    chore(001:app): add bare NextJS template

---
+++ 001-favorite-colors/src/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Favorite Colors</title>
+        <meta name="description" content="A list of my favorite colors" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>We got ourselves an app!</div>
+      </main>
+    </>
+  )
+}
