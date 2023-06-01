7f2b92fb2ef1355e78d0eb26b8e28eee19fb8da9
Andrew Pethoud <andrewpethoud@gmail.com>
2023-05-31 20:50:31 -0700

    feat(001:app): add title to view

--- 001-favorite-colors/src/pages/index.js
+++ 001-favorite-colors/src/pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main style={{ padding: 24 }}>
+        <div style={{ fontSize: 24, marginBottom: 24 }}>Favorite Colors</div>
         <ColorChart />
       </main>
     </>
