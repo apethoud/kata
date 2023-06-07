b2f18f8d3a54520eb09974c216f077d34e00e9eb
Andrew Pethoud <andrewpethoud@gmail.com>
2023-06-07 15:16:55 -0700

    wire up MovieDetail page

--- 004-movie-list/src/app/MovieCard.jsx
+++ 004-movie-list/src/app/MovieCard.jsx
@@ -1,19 +1,22 @@
 'use client';
 
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function MovieCard({ movie }) {
   return (
     <div style={{ marginRight: 24 }}>
-      <Image
-        src={movie.Poster}
-        width={200}
-        height={200}
-        alt="Picture of the movie poster"
-        priority={true}
-      />
-      <div>{movie.Title}</div>
-      <div>{movie.Year}</div>
+      <Link href={`/movie/${movie.imdbID}`}>
+        <Image
+          src={movie.Poster}
+          width={200}
+          height={200}
+          alt="Picture of the movie poster"
+          priority={true}
+        />
+        <div>{movie.Title}</div>
+        <div>{movie.Year}</div>
+      </Link>
     </div>
   )
 }
