651c87af15d325e2ffb5d38bec6add423745d936
Andrew Pethoud <andrewpethoud@gmail.com>
2023-06-07 18:04:50 -0700

    add supabase db with NPC aspect tables

---
+++ 005-npc-list/supabase/seed.sql
@@ -0,0 +1,23 @@
+INSERT INTO attitudes(name)
+VALUES
+  ('kind'),
+  ('scared'),
+  ('bored'),
+  ('guarded'),
+  ('rude');
+
+INSERT INTO names(name)
+VALUES
+  ('Duren'),
+  ('Velora'),
+  ('Bo'),
+  ('Cassandra'),
+  ('Mina');
+
+INSERT INTO races(name)
+VALUES
+  ('Human'),
+  ('Elf'),
+  ('Dwarf'),
+  ('Dragonborn'),
+  ('Gnome');
